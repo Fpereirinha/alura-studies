d77c2c8e06de23341fa78f1bc8270b7960ecbdcf
Pedro Fernandes <pedrofpereira222@gmail.com>
2022-12-15 13:08:01 -0300

    relogio

--- src/components/cronometro/index.tsx
+++ src/components/cronometro/index.tsx
@@ -2,22 +2,23 @@ import Button from "../button";
 import Relogio from "./relogio";
 import style from "./Cronometro.module.scss";
 import { ITarefa } from "../../types/tarefa";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { tempoParaSegundos } from "../common/time";
 interface Props {
 	selecionado: ITarefa | undefined;
 }
 export default function Cronometro({ selecionado }: Props) {
 	const [tempo, setTempo] = useState<number>();
-	if (selecionado?.tempo) {
-		setTempo(tempoParaSegundos(selecionado.tempo));
-	}
+	useEffect(() => {
+		if (selecionado?.tempo) {
+			setTempo(tempoParaSegundos(selecionado.tempo));
+		}
+	}, [selecionado]);
 	return (
 		<div className={style.cronometro}>
 			<p className={style.titulo}>Escolha um card e inicie o cronometro</p>
-			Tempo: {tempo}
 			<div className={style.relogioWrapper}>
-				<Relogio />
+				<Relogio tempo={tempo} />
 			</div>
 			<Button>Começar</Button>
 		</div>
--- src/components/cronometro/relogio/index.tsx
+++ src/components/cronometro/relogio/index.tsx
@@ -1,12 +1,19 @@
 import style from "./Relogio.module.scss";
-export default function Relogio() {
+interface Props {
+	tempo: number | undefined;
+}
+export default function Relogio({ tempo = 0 }: Props) {
+	const minutos = Math.floor(tempo / 60);
+	const segundos = tempo % 60;
+	const [minDez, minUn] = String(minutos).padStart(2, "0");
+	const [segDez, segUn] = String(segundos).padStart(2, "0");
 	return (
 		<>
-			<span className={style.relogioNumero}>0</span>
-			<span className={style.relogioNumero}>0</span>
+			<span className={style.relogioNumero}>{minDez}</span>
+			<span className={style.relogioNumero}>{minUn}</span>
 			<span className={style.relogioDivisao}>:</span>
-			<span className={style.relogioNumero}>0</span>
-			<span className={style.relogioNumero}>0</span>
+			<span className={style.relogioNumero}>{segDez}</span>
+			<span className={style.relogioNumero}>{segUn}</span>
 		</>
 	);
 }
