17a321c11ee17986d0a3544af0837068c9fe9b97
Pedro Fernandes <pedrofpereira222@gmail.com>
2022-12-19 16:14:42 -0300

    new

--- src/components/button/index.tsx
+++ src/components/button/index.tsx
@@ -1,18 +1,15 @@
 import React from "react";
 import style from "./Button.module.sass";
-class Button extends React.Component<{
+interface Props {
+	onclick?: () => void;
 	children?: React.ReactNode;
 	type?: "button" | "submit" | "reset" | undefined;
-	onclick?: () => void;
-}> {
-	render() {
-		const { type = "button", onclick } = this.props;
-		const { children = "Botão" } = this.props;
-		return (
-			<button type={type} className={style.botao} onClick={onclick}>
-				{children}
-			</button>
-		);
-	}
+}
+function Button({ onclick, type, children }: Props) {
+	return (
+		<button type={type} className={style.botao} onClick={onclick}>
+			{children}
+		</button>
+	);
 }
 export default Button;
--- src/components/form/index.tsx
+++ src/components/form/index.tsx
@@ -3,7 +3,55 @@ import Button from "../button";
 import style from "./Form.module.sass";
 import { ITarefa } from "../../types/tarefa";
 import { v4 as uuidv4 } from "uuid";
-class Form extends React.Component<{
+import { useState } from "react";
+interface Props {
+	setTarefas: React.Dispatch<React.SetStateAction<ITarefa[]>>;
+}
+function Form({ setTarefas }: Props) {
+	const [tarefa, setTarefa] = useState("");
+	const [tempo, setTempo] = useState("00:00");
+	function saveTarefa(evento: FormEvent<HTMLFormElement>) {
+		evento.preventDefault();
+		setTarefas((tarefasAntigas) => [
+			...tarefasAntigas,
+			{ tarefa, tempo, selecionado: false, completado: false, id: uuidv4() },
+		]);
+		setTarefa("");
+		setTempo("00:00");
+	}
+	return (
+		<form className={style.novaTarefa} onSubmit={saveTarefa}>
+			<div className={style.inputContainer}>
+				<label htmlFor="tarefa">Adicione um novo estudo</label>
+				<input
+					type="text"
+					name="tarefa"
+					id="tarefa"
+					value={tarefa}
+					onChange={(Evento) => setTarefa(Evento.target.value)}
+					placeholder="Oque irá estudar ?"
+					required
+				/>
+			</div>
+			<div className={style.inputContainer}>
+				<label htmlFor="time">Tempo</label>
+				<input
+					type="time"
+					id="time"
+					name="time"
+					required
+					value={tempo}
+					onChange={(Evento) => setTempo(Evento.target.value)}
+					step="1"
+					min="00:00:00"
+					max="01:30:00"
+				/>
+			</div>
+			<Button type="submit">Adicionar</Button>
+		</form>
+	);
+}
+class Form1 extends React.Component<{
 	setTarefas: React.Dispatch<React.SetStateAction<ITarefa[]>>;
 }> {
 	state = { tarefa: "", tempo: "00:00" };
